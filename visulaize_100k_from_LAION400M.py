823a2347c0e9688731110bafee0ff17d948b29cc
LukaLake <861053440@qq.com>
2023-05-05 19:17:56 +0800

    Update visulaize_100k_from_LAION400M.py

--- visulaize_100k_from_LAION400M.py
+++ visulaize_100k_from_LAION400M.py
@@ -14,6 +14,7 @@ import torch
 import pytorch_lightning as pl
 import torch.nn as nn
 from torchvision import datasets, transforms
+import torch.nn.functional as F
 import tqdm
 
 from os.path import join
